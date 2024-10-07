ee315de09620310808a45a64fb44c5194aa3205b
Muhammed-Riyas-ebslon <148744494+Muhammed-Riyas-ebslon@users.noreply.github.com>
2024-10-07 16:09:55 +0530

    swagger fixed

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ import logger from "morgan";
 import mongoose from "mongoose";
 import cors from "cors";
 import v1Router from "./v1/router.v1";
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
+// import swaggerJSDoc from "swagger-jsdoc";
+// import swaggerUi from "swagger-ui-express";
 
 import { API_PATHS, CONFIG } from "common/config.common";
 import { errorHandler } from "middlewares/errorHandler.middleware";
@@ -20,34 +20,34 @@ const app = express();
  * @description Swagger/OpenAi Config
  */
 
-const options = {
-  definition: {
-    openapi: "3.0.1",
-    info: {
-      title: "CREANDO",
-      version: "1.0.0",
-    },
-    schemes: ["http", "https"],
-    servers: [{ url: CONFIG.URL }],
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: "http",
-          scheme: "bearer",
-          bearerFormat: "JWT",
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
-  },
-  apis: API_PATHS,
-};
+// const options = {
+//   definition: {
+//     openapi: "3.0.1",
+//     info: {
+//       title: "CREANDO",
+//       version: "1.0.0",
+//     },
+//     schemes: ["http", "https"],
+//     servers: [{ url: CONFIG.URL }],
+//     components: {
+//       securitySchemes: {
+//         bearerAuth: {
+//           type: "http",
+//           scheme: "bearer",
+//           bearerFormat: "JWT",
+//         },
+//       },
+//     },
+//     security: [
+//       {
+//         bearerAuth: [],
+//       },
+//     ],
+//   },
+//   apis: API_PATHS,
+// };
 
-const swaggerSpec = swaggerJSDoc(options);
+// const swaggerSpec = swaggerJSDoc(options);
 
 /**
  * @description mongo connection and seeder
@@ -69,7 +69,7 @@ seedData();
 
 app.use(cors());
 app.set("trust proxy", true);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(setUserAndUserObj);
 app.use(logger("dev"));
 app.use(express.json({ limit: "500mb" }));
